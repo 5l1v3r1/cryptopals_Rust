1464ba5362764791f3477fabf36bbd4f5f0ed814
PinkP4nther <0x0090@protonmail.com>
2020-02-09 21:32:19 -0800

    chall 7 :D Learning Rust crypto library

---
+++ s1c7/src/main.rs
@@ -0,0 +1,26 @@
+extern crate crypto;
+extern crate base64;
+
+use crypto::aes::{self,KeySize};
+use crypto::blockmodes::NoPadding;
+use crypto::buffer::{RefReadBuffer,RefWriteBuffer};
+
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+
+    let mut decryptor = aes::ecb_decryptor(KeySize::KeySize128, b"YELLOW SUBMARINE",NoPadding);
+
+    let mut f = File::open("7.txt").unwrap();
+    let mut buffer = Vec::<u8>::new();
+    f.read_to_end(&mut buffer);
+
+    let mut outbuf: [u8; 4096] = [0;4096];
+    let mut inbuf = base64::decode(&buffer).unwrap();
+
+    println!("[+] Decrypting message...");
+    decryptor.decrypt(&mut RefReadBuffer::new(&mut inbuf), &mut RefWriteBuffer::new(&mut outbuf),true).unwrap();
+
+    println!("[+] Decrypted Message:\n\n{}",String::from_utf8(outbuf.to_vec()).unwrap().trim());
+}
